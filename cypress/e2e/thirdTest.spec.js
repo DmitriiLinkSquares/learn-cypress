f3281e40eac9ada97bbbd47d21977fa13347d059
angielix <angieiksanova@gmail.com>
2022-12-28 13:26:25 -0500

    Web tables, added the 3d example

--- cypress/e2e/thirdTest.spec.js
+++ cypress/e2e/thirdTest.spec.js
@@ -243,14 +243,46 @@ describe('Our first suite', () => {
     cy.contains('Tables & Data').click();
     cy.contains('Smart Table').click();
 
-    // 1 example
-    cy.get('tbody')
-      .contains('tr', 'Larry')
-      .then((tableRow) => {
-        cy.wrap(tableRow).find('.nb-edit').click();
-        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25');
-        cy.wrap(tableRow).find('.nb-checkmark').click();
-        cy.wrap(tableRow).find('td').eq(6).should('contain', '25');
+    // // 1 example
+    // cy.get('tbody')
+    //   .contains('tr', 'Larry')
+    //   .then((tableRow) => {
+    //     cy.wrap(tableRow).find('.nb-edit').click();
+    //     cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25');
+    //     cy.wrap(tableRow).find('.nb-checkmark').click();
+    //     cy.wrap(tableRow).find('td').eq(6).should('contain', '25');
+    //   });
+
+    // // 2 example
+    // cy.get('thead').find('.nb-plus').click();
+    // cy.get('thead')
+    //   .find('tr')
+    //   .eq(2)
+    //   .then((tableRow) => {
+    //     cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem');
+    //     cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar');
+    //     cy.wrap(tableRow).find('.nb-checkmark').click();
+    //     cy.get('tbody tr')
+    //       .first('tr')
+    //       .find('td')
+    //       .then((tableColumns) => {
+    //         cy.wrap(tableColumns).eq(2).should('contain', 'Artem');
+    //         cy.wrap(tableColumns).eq(3).should('contain', 'Bondar');
+    //       });
+    //   });
+
+    // 3 example
+    const age = [20, 30, 40, 200];
+    cy.wrap(age).each((age) => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500);
+      cy.get('tbody tr').each((tr) => {
+        if (age >= 60) {
+          cy.wrap(tr).should('contain', 'No data found');
+        } else {
+          cy.wrap(tr).find('td').eq(6).should('contain', age);
+        }
       });
+    });
   });
 });
