9015712de881acd86c45a6a2f80e45c844004f9c
angielix <121139467+angielix@users.noreply.github.com>
2022-12-29 14:38:48 -0500

    Lesson 3 (#4)
    * Hysky removed
    
    * Web tables test (fixed the 1st example)
    
    * Web tables, added the 3d example
    
    * saved package.json changes
    
    * craeted a Web datepickers test
    
    * created test for Tooltips
    
    * created test Dialog window
    
    * made changes to Invoke command test
    
    * changed the last assertion in Web datepickers test

--- cypress/e2e/thirdTest.spec.js
+++ cypress/e2e/thirdTest.spec.js
@@ -39,7 +39,6 @@ describe('Our first suite', () => {
     // The most recommended way by Cypress is to craete your own attribute
     cy.get('[data-cy="imputEmail1"]');
   });
-
   it('The second test', () => {
     // in order to open our app in cy we need to execute command
     cy.visit('/');
@@ -67,7 +66,6 @@ describe('Our first suite', () => {
 
     cy.contains('nb-card', 'Horizontal form').find('[type="email"]');
   });
-
   it('Then and wrap methods', () => {
     // in order to open our app in cy we need to execute command
     cy.visit('/');
@@ -121,7 +119,6 @@ describe('Our first suite', () => {
       });
     });
   });
-
   it('Invoke command', () => {
     // in order to open our app in cy we need to execute command
     cy.visit('/');
@@ -129,19 +126,22 @@ describe('Our first suite', () => {
     cy.contains('Form Layouts').click();
 
     // 1. to get text value
-    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address');
+    cy.get('[for="exampleInputEmail1"]')
+      .should('contain', 'Email address')
+      .should('have.class', 'label')
+      .and('have.text', 'Email address');
 
     // 2. to get text value using then()
     cy.get('[for="exampleInputEmail1"]').then((inputLabel) => {
       expect(inputLabel.text()).to.equal('Email address');
+      expect(inputLabel).to.have.class('label');
+      expect(inputLabel).to.have.text('Email address');
     });
 
     // 3. to get text value using invoke command
     cy.get('[for="exampleInputEmail1"]')
       .invoke('text')
-      .then((text) => {
-        expect(text).to.equal('Email address');
-      });
+      .then((text) => expect(text).to.equal('Email address'));
 
     // to verify that checkbox is actually checked
     cy.contains('nb-card', 'Basic form')
@@ -238,17 +238,126 @@ describe('Our first suite', () => {
       });
     });
   });
-  it.only('Web tables', () => {
+  it('Web tables', () => {
     cy.visit('/');
     cy.contains('Tables & Data').click();
     cy.contains('Smart Table').click();
+
+    // 1 example
     cy.get('tbody')
       .contains('tr', 'Larry')
       .then((tableRow) => {
         cy.wrap(tableRow).find('.nb-edit').click();
-        cy.wrap(tableRow).find('[placeholder="Age"]').type('25');
+        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25');
         cy.wrap(tableRow).find('.nb-checkmark').click();
-        cy.wrap(tableRow).find('tb').eq(6).should('contain');
+        cy.wrap(tableRow).find('td').eq(6).should('contain', '25');
       });
+
+    // 2 example
+    cy.get('thead').find('.nb-plus').click();
+    cy.get('thead')
+      .find('tr')
+      .eq(2)
+      .then((tableRow) => {
+        cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem');
+        cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar');
+        cy.wrap(tableRow).find('.nb-checkmark').click();
+        cy.get('tbody tr')
+          .first('tr')
+          .find('td')
+          .then((tableColumns) => {
+            cy.wrap(tableColumns).eq(2).should('contain', 'Artem');
+            cy.wrap(tableColumns).eq(3).should('contain', 'Bondar');
+          });
+      });
+
+    // 3 example
+    const age = [20, 30, 40, 200];
+    cy.wrap(age).each((age) => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500);
+      cy.get('tbody tr').each((tr) => {
+        if (age >= 60) {
+          cy.wrap(tr).should('contain', 'No data found');
+        } else {
+          cy.wrap(tr).find('td').eq(6).should('contain', age);
+        }
+      });
+    });
+  });
+  it('Web datepickers', () => {
+    function selectDayFromCurrent(day) {
+      let date = new Date(); // this object is getting current system date and time
+      date.setDate(date.getDate() + day);
+      let futureDay = date.getDate();
+      let futureMonth = date.toLocaleString('en-US', { month: 'short' });
+      let futureYear = date.getFullYear();
+      let dateAssert =
+        futureMonth + ' ' + futureDay + ', ' + date.getFullYear();
+
+      cy.get('nb-calendar-navigation')
+        .invoke('attr', 'ng-reflect-date')
+        .then((dateAttr) => {
+          if (
+            !dateAttr.includes(futureMonth) ||
+            !dateAttr.includes(futureYear)
+          ) {
+            cy.get('[data-name="chevron-right"]').click();
+            selectDayFromCurrent(day);
+          } else {
+            cy.get('nb-calendar-day-picker .ng-star-inserted') // it was because of locator
+              .contains(futureDay)
+              .click(); // returns cy.click() can only be called on a single element. Your subject contained 3 elements. Pass { multiple: true } if you want to serially click each element.
+          }
+        });
+      return dateAssert;
+    }
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Datepicker').click();
+
+    cy.contains('nb-card', 'Common Datepicker')
+      .find('input')
+      .then((input) => {
+        cy.wrap(input).click();
+        let dateAssert = selectDayFromCurrent(40);
+        // cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert);
+        cy.wrap(input).should('have.value', dateAssert); // do exactly the same as the line above
+      });
+  });
+  it('ToolTip', () => {
+    cy.visit('/');
+    cy.contains('Modal & Overlays').click();
+    cy.contains('Tooltip').click();
+    cy.contains('nb-card', 'Colored Tooltips').contains('Default').click();
+    cy.get('nb-tooltip').should('contain', 'This is a tooltip');
+  });
+  it('Dialog window', () => {
+    cy.visit('/');
+    cy.contains('Tables & Data').click();
+    cy.contains('Smart Table').click();
+
+    // method 1 (don't use because the code will not fail even if don't get any confirm message)
+    cy.get('tbody tr').first().find('.nb-trash').click();
+    cy.on('window:confirm', (confirm) => {
+      expect(confirm).to.equal('Are you sure you want to delete?');
+    });
+
+    // method 2
+    const stub = cy.stub();
+    cy.on('window:confirm', stub);
+    cy.get('tbody tr')
+      .first()
+      .find('.nb-trash')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(
+          'Are you sure you want to delete?'
+        );
+      });
+
+    // method 3 (if you want to select Cancel instead of Confirm)
+    cy.get('tbody tr').first().find('.nb-trash').click();
+    cy.on('window:confirm', () => false);
   });
 });
